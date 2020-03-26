817266f5a2493e18488ed8c5685ff98a480c7c16
rudirahardian <rudi.rahardian@ralali.com>
2020-03-26 17:39:57 +0700

    add create table user query

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user (
+    id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    username varchar(255) NOT NULL,
+    password varchar(255),
+    name varchar(255),
+    foto varchar(255)
+);
